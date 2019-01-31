32ad4ea992f56a4204935a2f3383a11148d9433e
ranjayzheng <ranjayzheng@qq.com>
2019-01-31 23:43:12 +0800

    add comments of resolvers

--- app.js
+++ app.js
@@ -73,6 +73,11 @@ const typeDefs = gql `
 // }
 
 // Provide resolver functions for your schema fields
+// 每个resolvers会传入四个参数：
+//1⃣️obj: 上一级对象，如果字段属于根节点则为undefined。(常用)
+//2⃣️args: 可以提供在 GraphQL 查询中传入的参数，即查询条件。(常用)
+//3⃣️context: 会被提供给所有解析器，并且持有重要的上下文信息比如当前登入的用户或者数据库访问对象。
+//4⃣️info: 一个保存与当前查询相关的字段特定信息以及 schema 详细信息的值，
 const resolvers = {
   Query: {
     user: (_, args) => {
